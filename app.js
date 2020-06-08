3ed60c66faf26f643b74d6a0c45dc47a742a5864
Nick Tk <nico.toccane@gmail.com>
2020-06-08 16:35:03 +0200

    fix cors configuration

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ const { validateRequest } = require("./middleware");
 app.use(validateRequest);
 app.use(async(req, res, next)=>{
   // CORS configuration
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", process.env.CLIENT_URL);
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
  	res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   next();
